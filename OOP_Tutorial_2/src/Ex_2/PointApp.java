e6a1cfe5f3de14afedfb0abcf13e15b7c0b50956
Yasira Banuka <111946114+YasiraBanuka@users.noreply.github.com>
2022-10-15 12:20:05 +0530

    Update PointApp.java

--- OOP_Tutorial_2/src/Ex_2/PointApp.java
+++ OOP_Tutorial_2/src/Ex_2/PointApp.java
@@ -1,11 +1,11 @@
-package Tutorial_2.Ex_2;
+package Ex_2;
 
 public class PointApp {
 
     public static void main(String[] args) {
 
-       Point3D ob1 = new Point3D(2, 3, 4);
-       ob1.distance();
+        Point3D ob1 = new Point3D(2, 3, 4);
+        ob1.distance();
 
         System.out.println("Distance : " + ob1.distance());
 
